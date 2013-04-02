df05bb4ab771939d1aadfadbde95dbf3d0e5e302
Ryan Scheel <scheel.ryan@gmail.com>
2013-04-02 00:38:23 -0500

    made a mistake with the update all command. Should only be 'sudo yum update'.

--- fedora/freshInstall.sh
+++ fedora/freshInstall.sh
@@ -1,4 +1,4 @@
-sudo -y yum update all
+sudo -y yum update
 sudo -y yum install vim-X11 vim-common vim-enhanced vim-minimal
 sudo -y yum groupinstall 'Development Tools'
 sudo cp google-chrome.repo /etc/yum.repos.d/
