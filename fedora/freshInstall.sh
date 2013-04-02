d0e057575f6dae9bb8f2c06d69100314abd2f411
Ryan Scheel <rascheel@localhost.localdomain>
2013-04-02 00:30:27 -0500

    added fresh fedora install setup.

---
+++ fedora/freshInstall.sh
@@ -0,0 +1,5 @@
+sudo -y yum update all
+sudo -y yum install vim-X11 vim-common vim-enhanced vim-minimal
+sudo -y yum groupinstall 'Development Tools'
+sudo cp google-chrome.repo /etc/yum.repos.d/
+sudo -y yum install google-chrome-stable
