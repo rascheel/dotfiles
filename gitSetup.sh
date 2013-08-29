fe5a55475199cc6cca72e15c9741319e009b8a63
Ryan Scheel <scheel.ryan@gmail.com>
2013-08-29 10:02:45 -0500

    Updated git setup

--- gitSetup.sh
+++ gitSetup.sh
@@ -4,3 +4,5 @@ ssh-keygen -t rsa -C "scheel.ryan@gmail.com" -q -N "" -f ~/.ssh/id_rsa
 sudo apt-get -y install xclip
 xclip -sel clip < ~/.ssh/id_rsa.pub
 echo "github ssh key copied to clipboard, add to github now."
+git remote set-url origin git@github.com:rascheel/dotfiles.git
+echo "Changed git remote url to the ssh url, in case I probably used https."
