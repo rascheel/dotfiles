b41e87bc6e458502dd2580cdf998ae97557316ca
Ryan Scheel <scheel.ryan@gmail.com>
2013-08-29 09:56:58 -0500

    Added git setup and fresh ubuntu install setups

---
+++ ubuntu/freshInstall.sh
@@ -0,0 +1,11 @@
+sudo apt-get -y update
+sudo apt-get -y upgrade
+
+sudo apt-get -y install build-essential
+
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+cp google.list /etc/apt/sources.list.d/
+sudo apt-get -y update
+sudo apt-get -y install google-chrome-stable
+
+sudo apt-get -y install vim-gnome vim
