783cfc2cf8cbf2b495f3b30171427df5837f484f
Ryan Scheel <scheel.ryan@gmail.com>
2013-08-29 19:44:20 -0500

    Forgot to add .gitignore and to save a file before last commit.

--- gitSetup.sh
+++ gitSetup.sh
@@ -1,7 +1,6 @@
 git config --global user.name "Ryan Scheel"
 git config --global user.email "scheel.ryan@gmail.com"
 ssh-keygen -t rsa -C "scheel.ryan@gmail.com" -q -N "" -f ~/.ssh/id_rsa
-sudo apt-get -y install xclip
 xclip -sel clip < ~/.ssh/id_rsa.pub
 echo "github ssh key copied to clipboard, add to github now."
 git remote set-url origin git@github.com:rascheel/dotfiles.git
