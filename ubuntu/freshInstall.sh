633bcc238a9d6983da881de9fae9d4a029704a95
Ryan Scheel <scheel.ryan@gmail.com>
2013-09-05 10:45:20 -0500

    fixed a bug in fresh install.

--- ubuntu/freshInstall.sh
+++ ubuntu/freshInstall.sh
@@ -4,7 +4,7 @@ sudo apt-get -y upgrade
 sudo apt-get -y install build-essential
 
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-cp google.list /etc/apt/sources.list.d/
+sudo cp google.list /etc/apt/sources.list.d/
 sudo apt-get -y update
 sudo apt-get -y install google-chrome-stable
 
