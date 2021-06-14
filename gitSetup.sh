95f796f0c145453814182d18ffd72306fd7a47a5
Ryan Scheel <scheel.ryan@gmail.com>
2021-06-14 18:12:29 -0500

    Set neovim as my primary git editor

--- gitSetup.sh
+++ gitSetup.sh
@@ -1,5 +1,6 @@
 git config --global user.name "Ryan Scheel"
 git config --global user.email "scheel.ryan@gmail.com"
+git config --global core.editor "nvim"
 ssh-keygen -t rsa -C "scheel.ryan@gmail.com" -q -N "" -f ~/.ssh/id_rsa
 xclip -sel clip < ~/.ssh/id_rsa.pub
 echo "github ssh key copied to clipboard, add to github now."
