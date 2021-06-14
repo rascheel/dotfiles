8b8a090e2ec09ac3b2d87693bf356cf9d233af5d
Ryan Scheel <scheel.ryan@gmail.com>
2021-06-14 18:33:22 -0500

    Changed configure.sh to create symlink to init.vim
    This way I can just edit my dotfiles directly and not worry about the
    local init.vim getting out of sync one way or the other.

--- configure.sh
+++ configure.sh
@@ -1,5 +1,5 @@
 mkdir -p ~/.config/nvim
-cp ~/git/dotfiles/init.vim ~/.config/nvim/
+ln -s ~/git/dotfiles/init.vim ~/.config/nvim/init.vim
 
 tee --append ~/.bashrc < ~/git/dotfiles/alias
 echo ".bashrc modified, restart terminal (or source .bashrc)"
