98e74101dec25f2ff750f3f0d5d54817b80159ea
Ryan Scheel <scheel.ryan@gmail.com>
2021-06-21 11:00:41 -0500

    Update install libraries for build/doc tools

--- ubuntu/freshInstall.sh
+++ ubuntu/freshInstall.sh
@@ -7,12 +7,8 @@ sudo apt-get -y install build-essential
 
 sudo apt-get -y install xclip
 
-sudo apt-get -y install exuberant-ctags
-
-sudo apt-get -y install tmux tmuxinator
-
-#Install ripgrep because its rip-diculously faster than grep
-sudo apt-get -y install ripgrep
+# Tools I like
+sudo apt-get -y install tmux tmuxinator tree exuberant-ctags ripgrep
 
 # Neovim python dependencies
 sudo apt-get -y install python3-pip
@@ -35,3 +31,9 @@ sudo make install
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 nvim +PlugInstall +qall
+
+# Install build dependencies
+sudo apt-get -y install libncurses5-dev gcc make git exuberant-ctags bc libssl-dev
+
+# Documentation tools
+sudo apt-get -y install sphinxsearch python3-docutils python3-sphinx python3-sphinx-rtd-theme graphviz imagemagick texlive-latex-recommended texlive-base 
