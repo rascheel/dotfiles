dab8e4fd1d9d377d25a831287e1758222e45a121
Ryan Scheel <scheel.ryan@gmail.com>
2021-12-03 12:49:54 -0600

    Moved vim plugin install to configure script that should be run after a fresh install

--- configure.sh
+++ configure.sh
@@ -1,5 +1,12 @@
+# Setup vim config
 mkdir -p ~/.config/nvim
 ln -s ~/git/dotfiles/init.vim ~/.config/nvim/init.vim
 
+# Install neovim plugins
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+nvim +PlugInstall +qall
+
+# Add aliases
 tee --append ~/.bashrc < ~/git/dotfiles/alias
 echo ".bashrc modified, restart terminal (or source .bashrc)"
--- ubuntu/freshInstall.sh
+++ ubuntu/freshInstall.sh
@@ -27,11 +27,6 @@ sudo apt-get -y install ninja-build gettext libtool libtool-bin autoconf automak
 make CMAKE_BUILD_TYPE=Release
 sudo make install
 
-# Install neovim plugins
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-nvim +PlugInstall +qall
-
 # Install build dependencies
 sudo apt-get -y install libncurses5-dev gcc make git exuberant-ctags bc libssl-dev
 
