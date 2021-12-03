04acda25b0d250df7f29d10fa2291c43ced61e1f
Ryan Scheel <scheel.ryan@gmail.com>
2021-12-03 12:46:43 -0600

    Missed a -y in the freshInstall script

--- ubuntu/freshInstall.sh
+++ ubuntu/freshInstall.sh
@@ -23,7 +23,7 @@ cd ~/git
 git clone https://github.com/neovim/neovim.git
 cd neovim
 git checkout stable
-sudo apt-get install ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip
+sudo apt-get -y install ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip
 make CMAKE_BUILD_TYPE=Release
 sudo make install
 
