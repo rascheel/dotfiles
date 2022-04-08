17b10bafa3909f1aeb906b23680506a3b5e989c5
Ryan Scheel <scheel.ryan@gmail.com>
2022-04-08 10:52:27 -0700

    Add tmux themes

--- scripts/configure.sh
+++ scripts/configure.sh
@@ -4,6 +4,7 @@ ln -s ~/git/dotfiles/init.vim ~/.config/nvim/init.vim
 
 # Setup tmux config
 ln -s ~/git/dotfiles/tmux.conf ~/.tmux.conf
+git clone https://github.com/jimeh/tmux-themepack.git ~/.tmux-themepack
 
 # Install neovim plugins
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
